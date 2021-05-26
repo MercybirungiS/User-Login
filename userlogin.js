29ba9e78b4351d1335057b80b266d3a4b2dd67bb
uwasejeannine <jeannineuwasee@gmail.com>
2021-05-26 19:49:48 +0300

    userlogin

--- userlogin.js
+++ userlogin.js
@@ -1,13 +1,11 @@
-function userName(firstname , lastname){
-    console.log(`${firstname},${lastname}`)
-}
 
-function loginUser(email,password,callback){
+function loginUser(firstname,lastname,email,password,ID,callback){
     setTimeout(()=>{
         console.log("Now we have your information");
-        callback({userEmail:email});
+        callback({userFN:firstname,userLN:lastname,userEmail:email,userPassword:password,userID:ID});
 
     },3000)}
+   
     function userVideos(email,callback){
         setTimeout(()=>{
             callback(["video1","video2","video3"]);
@@ -17,11 +15,13 @@ function loginUser(email,password,callback){
     
 
     
-    const user =loginUser("mercyb@gmail.com",68888,user=>{
+    const user =loginUser("Mercy","Birungi","mercyb@gmail.com",68888,"103f",user=>{
         console.log(user);
         userVideos(user.userEmail,videos=>{
 console.log(videos);
         });
     });
-    userName("Mercy","Birungi")
+   
+    
+
     
